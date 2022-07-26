0bff629d8fd9b5cd45d26371d9b332a2c761fe12
kraze <krazex@naver.com>
2022-07-27 00:12:17 +0900

    0727

---
+++ semi_0727/main_1/main.js
@@ -0,0 +1,33 @@
+(function ($) {
+    "use strict";
+
+
+    // Sticky Navbar - navbar 상단에 붙이기
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 45) {
+            $('.nav-bar').addClass('sticky-top');
+        } else {
+            $('.nav-bar').removeClass('sticky-top');
+        }
+    });
+    
+
+
+    // Header carousel
+    $(".header-carousel").owlCarousel({
+        autoplay: true,
+        smartSpeed: 1500,
+        items: 1,
+        dots: true,
+        loop: true,
+        nav : true,
+        navText : [
+            '<i class="bi bi-chevron-left"></i>',
+            '<i class="bi bi-chevron-right"></i>'
+        ]
+    });
+
+
+    
+})(jQuery);
+
