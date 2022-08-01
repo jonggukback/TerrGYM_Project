5db19594cf4781c60ef86a9fad916664006fec53
kraze <krazex@naver.com>
2022-08-01 22:57:19 +0900

    0801 main_ver3

---
+++ main_ver3/main.js
@@ -0,0 +1,32 @@
+(function ($) {
+
+    
+    // // Initiate the wowjs
+    // new WOW().init();
+
+
+    // Sticky Navbar - navbar 상단에 붙이기
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 45) {
+            $('.nav-bar').addClass('sticky-top');
+        } else {
+            $('.nav-bar').removeClass('sticky-top');
+        }
+    });
+
+    
+    // Back to top button
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 300) {
+            $('.back-to-top').fadeIn('slow');
+        } else {
+            $('.back-to-top').fadeOut('slow');
+        }
+    });
+    $('.back-to-top').click(function () {
+        $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
+        return false;
+    });
+
+    
+})(jQuery);
